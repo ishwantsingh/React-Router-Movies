651847dc1c2490147381e4b4a009dc6b95b62b2a
Ishwant Singh <ishwant8@gmail.com>
2019-03-10 21:59:08 +0530

    feat: when clicking a movie title it opens its description. feat: home button takes back to movie list

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+
+import { Route } from "react-router-dom";
 
 export default class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       savedList: []
     };
@@ -22,7 +24,10 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <div>
+          <Route exact path="/" component={MovieList} />
+          <Route path="/movies/:id" component={Movie} />
+        </div>
       </div>
     );
   }
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React from "react";
 
-const MovieCard = props => {
-  return;
+import { Link } from "react-router-dom";
+
+const MovieCard = (props, movie) => {
+  const { title, director, metascore, stars } = props.movie;
+  return (
+    <div className="save-wrapper">
+      <Link to={`/movies/${props.movie.id}`}>
+        <div className="movie-card">
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+          <h3>Actors</h3>
+
+          {stars.map(star => (
+            <div key={star} className="movie-star">
+              {star}
+            </div>
+          ))}
+        </div>
+      </Link>
+      {/* {console.log(props.saveMovie)} */}
+      <div className="save-button">Save</div>
+    </div>
+  );
 };
 
 export default MovieCard;
